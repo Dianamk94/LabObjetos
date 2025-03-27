892121557ac94cc6d4481b287ae7dc7fde57c2a8
Diana <arq.dianamendez94@gmail.com>
2025-03-27 17:25:24 -0500

    nueva actualización de la actividad

--- termometro.js
+++ termometro.js
@@ -1,18 +1,22 @@
 //Termometro
 
-function Termometro() {
-    ;
-  }
-  Termometro.prototype.actualizar = function actualizar(grados) {
-    if (grados > 30) {
-      console.log(`Hace calor!`);
-    } else if (grados < 10) {
-      console.log(`Hace frio!`);
-    } else {
-      console.log(`Temperatura agradable!`);
-    }
-  }
+function Termometro() {this.temperatura = 0;
+
+    this.actualizar = function(grados) {
+        this.temperatura = grados;
+        
+        if (grados > 30) {
+            console.log("¡Hace calor!");
+        } else if (grados < 10) {
+            console.log("¡Hace frío!");
+        } else {
+            console.log("Temperatura agradable");
+        }
+    };
+}
+    
   let termometro1 = new Termometro();
+  
   termometro1.actualizar(5)
   termometro1.actualizar(20)
   termometro1.actualizar(35)
